540328b28d832078992a2f779f2df5539b1dd4e9
vladyka1989 <vladyka1989@gmail.com>
2016-05-10 01:02:46 +0300

    Add files via upload
    Адаптация официального модуля под Опенкарт 2.0
    Добавлена обработка статусов в ответе сервера. 
    Добавлена переадресация на страницы статусов success, failure, wait (кастомный дополнительный статус).
    Делал для личных нужд, доработки еще будут.

---
+++ catalog/language/english/checkout/wait.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title']        = 'Your order has been placed and waits for checking!';
+
+// Text
+$_['text_basket']          = 'Shopping Cart';
+$_['text_checkout']        = 'Checkout';
+$_['text_wait']         = 'Waiting';
+$_['text_customer']        = '<p>Your order has been successfully processed!</p><p>After checking you will get an e-mail with next instructions.</p>  You can check a status of your order by going to the <a href="%s">my account</a> page and by clicking on <a href="%s">history</a>.</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
+$_['text_guest']           = '<p>Your order has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for shopping with us online!</p>';
